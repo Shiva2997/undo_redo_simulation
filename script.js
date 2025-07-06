82328b7661e8f59c0d4ba57c7e1947e49a8b5988
Shiva Gupta <142409104+Shiva2997@users.noreply.github.com>
2025-07-06 17:41:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ let redoStack = [];
 // Save input history on typing
 editor.addEventListener("input", () => {
   undoStack.push(editor.value);
-  redoStack = []; // Clear redo stack when new input is made
+  redoStack = []; 
 });
 
 // Undo function
 function undo() {
   if (undoStack.length > 1) {
-    redoStack.push(undoStack.pop()); // Move last state to redo
-    editor.value = undoStack[undoStack.length - 1]; // Restore previous state
+    redoStack.push(undoStack.pop()); 
+    editor.value = undoStack[undoStack.length - 1]; 
   } else if (undoStack.length === 1) {
     redoStack.push(undoStack.pop());
     editor.value = "";
   }
 }
 
-// Redo function
+
 function redo() {
   if (redoStack.length > 0) {
     const nextState = redoStack.pop();
